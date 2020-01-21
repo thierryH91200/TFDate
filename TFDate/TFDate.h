6463d44704006d687590c1628916578530688562
thierryH91200 <thierry.hentic@gmail.com>
2020-01-21 07:49:44 +0100

    Initial Commit

---
+++ TFDate/TFDate.h
@@ -0,0 +1,19 @@
+//
+//  TFDate.h
+//  TFDate
+//
+//  Created by thierry hentic on 21/01/2020.
+//  Copyright © 2020 thierry hentic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TFDate.
+FOUNDATION_EXPORT double TFDateVersionNumber;
+
+//! Project version string for TFDate.
+FOUNDATION_EXPORT const unsigned char TFDateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFDate/PublicHeader.h>
+
+
